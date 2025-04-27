9f070e0684264cf0d89c83777ce61630e63c817f
0d-fako <od.fakorede@gmail.com>
2025-04-27 17:36:28 +0100

    CX Updates

--- cxhtml/node_intro/index.js
+++ cxhtml/node_intro/index.js
@@ -1,7 +1,13 @@
 const express = require('express');
 const app = express();
 
-const cors = require('cors');
 
-app.use(cors());
+const PORT = process.env.PORT || 7000;
+
+app.listen(PORT, ()=> {
+    console.log(`Server is running on port ${PORT}`);
+})
+// const cors = require('cors');
+
+// app.use(cors());
 
