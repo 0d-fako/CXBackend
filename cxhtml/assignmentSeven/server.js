fd6e6b3ed20f27b1e7757e0b388877b0279fff67
0d-fako <od.fakorede@gmail.com>
2025-05-08 14:10:41 +0100

    Assignment Seven update

---
+++ cxhtml/assignmentSeven/server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+const app = express();
+app.use(express.json());
+
+const cors = require("cors");
+app.use(cors());
+
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.error("MongoDB connection error:", err));
+
+app.listen(5000, () => console.log("Server running on port 5000"));
