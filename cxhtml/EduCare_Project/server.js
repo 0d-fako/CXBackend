11e760164a902730d8b74f64ceb5819e7412da8c
0d-fako <od.fakorede@gmail.com>
2025-05-15 00:03:48 +0100

    Fixed Server.js to import routes

--- cxhtml/EduCare_Project/server.js
+++ cxhtml/EduCare_Project/server.js
@@ -1,28 +1,34 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const cors = require("cors");
-const dotenv = require("dotenv").config();
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+
+import loginRoute from "./routes/auth/login.js";
+import signupRoute from "./routes/auth/signup.js";
+import courseRoute from "./routes/courseController.js";
+import enrollmentRoute from "./routes/enrollmentController.js";
+
+dotenv.config();
+
 
 const app = express();
-
 const PORT = process.env.PORT;
 
-mongooose
+
+mongoose
   .connect(process.env.MONGODB_URI)
-  .then(() => {
-    console.log("MongoDB connected");
-  })
-  .catch((err) => {
-    console.log(err);
-  });
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.error("MongoDB connection error:", err));
 
 app.use(cors());
-
 app.use(express.json());
 
-app.listen(PORT, () => {
-  console.log(`server is running on port ${PORT}`);
-});
+app.use("/api/auth/login", loginRoute);
+app.use("/api/auth/signup", signupRoute);
 
-const eduCareRoutes = require("./routes/eduCareRoutes");
-app.use("/api/eduCare", eduCareRoutes);
+app.use("/api/courses", courseRoute);
+app.use("/api/enrollments", enrollmentRoute);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
