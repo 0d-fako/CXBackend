dd9e00d8324af54154e58c36177ce29393ae69d8
0d-fako <od.fakorede@gmail.com>
2025-05-14 14:25:25 +0100

    Built User model

--- cxhtml/EduCare_Project/models/User.js
+++ cxhtml/EduCare_Project/models/User.js
@@ -1,4 +1,18 @@
 import mongoose from 'mongoose';
 import bcrypt from 'bcrypt';
 
-const userSchema = new mongoose.Schema({})
+const userSchema = mongoose.Schema({
+    name: {type: String, required: true},
+    email:{type: String, required: true},
+    password:{type: String, required: true}
+
+},
+    {
+        timestamps: true,
+        discriminatorKey: "role",
+    }
+
+)
+
+const User = mongoose.model("User", userSchema);
+export default User;
