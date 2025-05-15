d2ed2228e7629f2bce070bdd1f8fe474eb449bef
0d-fako <od.fakorede@gmail.com>
2025-05-15 06:40:58 +0100

    Implemented Auth routes

--- cxhtml/EduCare_Project/routes/auth/signup.js
+++ cxhtml/EduCare_Project/routes/auth/signup.js
@@ -20,7 +20,7 @@ router.post("/", async (req, res) => {
       (await Instructor.findOne({ email }));
 
     if (existingUser) {
-      return res.status(400).json({ message: "User already exist" });
+      return res.status(400).json({ message: "User already exists. Please provide a different email" });
     }
 
     const hashedPassword = await bcrypt.hash(password, 10);
@@ -49,7 +49,7 @@ router.post("/", async (req, res) => {
     });
   } catch (error) {
     console.error(error);
-    return res.status(500).json({ message: "Error Signing Up" });
+    return res.status(500).json({ message: "Error Signing Up, Internal Server Error" });
   }
 });
 
