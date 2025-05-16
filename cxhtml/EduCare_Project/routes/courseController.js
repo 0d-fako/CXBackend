956a84c3db2c3cdccae521c704033da662fbd6de
0d-fako <od.fakorede@gmail.com>
2025-05-16 11:23:25 +0100

    Implementing course controller

--- cxhtml/EduCare_Project/routes/courseController.js
+++ cxhtml/EduCare_Project/routes/courseController.js
@@ -7,27 +7,53 @@ import Course from "../models/Course.js";
 
 const router = express.Router();
 
-router.post("/createCourse", validateInstructor(), async (req, res) => {
+const validateInstructor = (req, res, next) => {
+  if (req.user?.role !== "Instructor") {
+    return res
+      .status(403)
+      .json({ message: "Access denied, Only instructor can create course" });
+  }
+  next();
+};
+
+router.post("/createCourse", validateInstructor, async (req, res) => {
   try {
-    const { courseCode, courseTitle, courseDescription, instructorid } =
+    const { courseCode, courseTitle, courseDescription, instructorId } =
       req?.body;
 
-    if (!courseCode || !instructorid || !courseTitle) {
+    if (!courseCode || !instructorId || !courseTitle) {
       return res.status(400).json({
         message: "Course code, course title, instructor are required",
       });
     }
 
-    
-
-    let existingCourse = await Course.findOne({ email });
+    let existingCourse = await Course.findOne({ courseCode });
 
     if (existingCourse) {
       return res.status(400).json({
         message: "Course code already exist",
       });
     }
+
+    let instructor = await Instructor.findById(instructorId);
+
+    if (!instructor) {
+      return res.status(404).json({ message: "Unable to find the instructor" });
+    }
+
+    const newCourse = new Course({
+      courseCode,
+      courseTitle,
+      courseDescription,
+      instructor: instructorId,
+    });
+
+    await newCourse.save();
+
+    return res
+      .status(201)
+      .json({ message: "Course created successfully", course: newCourse });
   } catch (error) {
-    return res.json(500).json({ message: "Error creating course" });
+    return res.status(500).json({ message: "Error creating course" });
   }
 });
